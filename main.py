7748d155f0e404fffb2ba66ec8978dd71ec7a50e
hadisupra <hadi.supra@gmail.com>
2025-10-20 20:24:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from streamlit import text_input
 import tiktoken
 from dotenv import load_dotenv
 from embedder import get_embedder
-from utils import load_and_chunk_csv
+# from utils import load_and_chunk_csv
 from langchain.chains import RetrievalQA
 from langchain_community.llms import OpenAI
 from langchain_community.vectorstores import Qdrant
